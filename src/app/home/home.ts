b0d948a91d0d39be855c6db049e72be1d43a76ee
sudomaster00081 <a.jayprasadd0008@gmail.com>
2025-07-20 22:14:51 +0530

    ui change

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import {  Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { signal } from '@angular/core';
@@ -12,26 +12,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home  {
+export class Home {
   carouselItems = [
     {
       title: 'Color Symphony',
       link: '/color-symphony',
+      img: 'https://images.unsplash.com/photo-1464983953574-0892a716854b?auto=format&fit=crop&w=400&q=80'
       // description: 'Give description' // optional
     },
     {
-      title: 'Bubble alphabet',
+      title: 'Bubble Alphabet',
       link: '/alphabet',
+      img: 'https://images.unsplash.com/photo-1503676382389-4809596d5290?auto=format&fit=crop&w=400&q=80'
       // description: 'Give description' // optional
     },
     {
       title: 'Emotion Explorer',
       link: '/emotion-match',
+      img: 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80'
       // description: 'Give description' // optional
     },
 
   ];
 
   protected readonly title = signal('Game Portal');
-  
+
 }
